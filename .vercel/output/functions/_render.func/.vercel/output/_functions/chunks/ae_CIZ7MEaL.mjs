105a50412c960681689d802e8dc92f869f4ee907
haydonge <haydonge@gmail.com>
2024-06-20 13:55:11 +0800

    index layout

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/ae_CIZ7MEaL.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/ae_CH2DiUZ8.mjs').then(n => n.b);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/auto_CJJBi5Ez.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/auto_C4oKWYX8.mjs');
+
+export { page };
