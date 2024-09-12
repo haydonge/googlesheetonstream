d13c8d72eb5eea079718a1bedfa8fe4ff69c02e0
Haydon Ge <93337428+haydonge@users.noreply.github.com>
2024-09-12 09:40:28 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), icon()]
 });
